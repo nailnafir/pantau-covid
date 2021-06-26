fc776f935797ac1744d7d76254066a06f9b19fb9
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-26 20:58:44 +0700

    add header main page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -15,17 +15,53 @@ class _MainPageState extends State<MainPage> {
       body: SafeArea(
         child: Stack(
           children: [
-            Row(
-              children: [
-                Text(
-                  "Pantau Covid",
-                  style: TypeTheme.titleTextFont,
-                ),
-              ],
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: SpaceConfig.longSpace,
+                vertical: SpaceConfig.shortSpace,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("PANTAU COVID",
+                      style: TypeTheme.normalTextFont.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      )),
+                  Icon(
+                    Icons.notifications_outlined,
+                    color: Colors.white,
+                  )
+                ],
+              ),
             ),
             Container(
-              width: LayoutConfig.deviceWidth,
-              height: 300,
+              margin: EdgeInsets.symmetric(
+                vertical: 50,
+                horizontal: SpaceConfig.longSpace,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Kami\nMau Sehat",
+                    style: TypeTheme.titleTextFont.copyWith(
+                      color: Colors.black.withOpacity(0.5),
+                      fontSize: 24,
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/image-virus.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
