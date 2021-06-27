39f8de292e05f113047932048ff784ae53771ecc
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-27 23:45:30 +0700

    add bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
+      home: MainPage(initialPage: 0),
     );
   }
 }
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,79 @@
+part of 'pages.dart';
+
+class MainPage extends StatefulWidget {
+  final int initialPage;
+
+  const MainPage({Key? key, required this.initialPage}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedPage = 0;
+  PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedPage = widget.initialPage;
+    _pageController = PageController(initialPage: widget.initialPage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorTheme.secondaryColor,
+      body: SafeArea(
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          children: [
+            HomePage(),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Beranda",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Kasus",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "Berita",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: "Bantuan",
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+        selectedLabelStyle:
+            TypeTheme.normalTextFont.copyWith(fontWeight: FontWeight.w500),
+        unselectedLabelStyle:
+            TypeTheme.normalTextFont.copyWith(fontWeight: FontWeight.w500),
+        selectedItemColor: ColorTheme.secondaryColor,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedPage,
+        elevation: 2,
+        onTap: (index) {
+          setState(() {
+            _selectedPage = index;
+          });
+          _pageController.jumpToPage(_selectedPage);
+        },
+      ),
+    );
+  }
+}
