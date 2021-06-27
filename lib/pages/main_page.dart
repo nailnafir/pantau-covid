c46efac8d27bc5cb3362fdde12d0d127a2958e7d
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-27 18:16:23 +0700

    add build body and header component

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -13,78 +13,89 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       backgroundColor: ColorTheme.primaryColor,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Positioned(
-              top: -130,
-              left: -130,
-              child: Bubble(
-                color: ColorTheme.secondaryColor,
-                width: 300,
-                height: 300,
-              ),
-            ),
-            Positioned(
-              top: 80,
-              right: -50,
-              child: Bubble(
-                color: ColorTheme.secondaryColor,
-                width: 100,
-                height: 100,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: SpaceConfig.longSpace,
-                vertical: SpaceConfig.shortSpace,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("PANTAU COVID",
-                      style: TypeTheme.normalTextFont.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      )),
-                  Icon(
-                    Icons.notifications_outlined,
-                    color: Colors.white,
-                  )
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                horizontal: SpaceConfig.longSpace,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    "Hilangkan\nCovid-19",
-                    style: TypeTheme.normalTextFont.copyWith(
-                      color: Colors.black.withOpacity(0.5),
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24,
-                    ),
-                  ),
-                  Container(
-                    width: 210,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image:
-                            AssetImage("assets/images/image-fight-virus.png"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: _buildBody(),
       ),
+    );
+  }
+
+  _buildBody() {
+    return ListView(
+      children: [
+        _header(),
+      ],
+    );
+  }
+
+  _header() {
+    return Stack(
+      children: [
+        Positioned(
+          top: -130,
+          left: -130,
+          child: Bubble(
+            color: ColorTheme.secondaryColor,
+            width: 300,
+            height: 300,
+          ),
+        ),
+        Positioned(
+          top: 80,
+          right: -50,
+          child: Bubble(
+            color: ColorTheme.secondaryColor,
+            width: 100,
+            height: 100,
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: SpaceConfig.longSpace,
+            vertical: SpaceConfig.shortSpace,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("PANTAU COVID",
+                  style: TypeTheme.normalTextFont.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                  )),
+              Icon(
+                Icons.notifications_outlined,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: SpaceConfig.longSpace,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Hilangkan\nCovid-19",
+                style: TypeTheme.normalTextFont.copyWith(
+                  color: Colors.black.withOpacity(0.5),
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24,
+                ),
+              ),
+              Container(
+                width: 210,
+                height: 250,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/image-fight-virus.png"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
