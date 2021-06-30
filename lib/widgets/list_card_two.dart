940b7435e132f928edebc5ac6ff7131d60a5d460
Nailul Firdaus <nail.nafir@gmail.com>
2021-06-30 19:36:53 +0700

    add elevation in list card two

--- lib/widgets/list_card_two.dart
+++ lib/widgets/list_card_two.dart
@@ -17,40 +17,45 @@ class ListCardTwoLines extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(SpaceConfig.normalSpace),
-      decoration: BoxDecoration(
-          color: tileColor,
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(SpaceConfig.normalSpace)),
-      child: ListTile(
-        leading: Container(
-          padding: EdgeInsets.all(SpaceConfig.shortSpace),
-          decoration: BoxDecoration(
-            color: backgroundIconColor,
-            shape: BoxShape.circle,
+      child: Container(
+        padding: EdgeInsets.all(SpaceConfig.normalSpace),
+        decoration: BoxDecoration(
+            color: tileColor,
+            borderRadius: BorderRadius.circular(SpaceConfig.normalSpace)),
+        child: ListTile(
+          leading: Container(
+            padding: EdgeInsets.all(SpaceConfig.shortSpace),
+            decoration: BoxDecoration(
+              color: backgroundIconColor,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              icon,
+              color: Colors.white,
+            ),
           ),
-          child: Icon(
-            icon,
-            color: Colors.white,
+          title: Text(
+            title,
+            style: TypeTheme.bigTextFont.copyWith(
+              color: Colors.black.withOpacity(0.75),
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
-        title: Text(
-          title,
-          style: TypeTheme.bigTextFont.copyWith(
+          subtitle: Text(
+            subtitle!,
+            style: TypeTheme.smallTextFont.copyWith(
+              color: Colors.black.withOpacity(0.75),
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios,
             color: Colors.black.withOpacity(0.75),
-            fontWeight: FontWeight.w600,
           ),
-        ),
-        subtitle: Text(
-          subtitle!,
-          style: TypeTheme.smallTextFont.copyWith(
-            color: Colors.black.withOpacity(0.75),
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        trailing: Icon(
-          Icons.arrow_forward_ios,
-          color: Colors.black.withOpacity(0.75),
         ),
       ),
     );
