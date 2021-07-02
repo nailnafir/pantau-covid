c74a561c66c7c7fa757122457e7023d52f12009e
Nailul Firdaus <nail.nafir@gmail.com>
2021-07-02 11:01:13 +0700

    change material app with get material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pantau_covid/pages/pages.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: MainPage(initialPage: 0),
     );
   }
