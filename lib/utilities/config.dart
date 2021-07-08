c3751b9a68aebf17afbe2266d176c0920abdf249
Nailul Firdaus <nail.nafir@gmail.com>
2021-07-08 08:34:59 +0700

    delete layout config in utilities

--- lib/utilities/config.dart
+++ lib/utilities/config.dart
@@ -1,17 +1,5 @@
 part of 'utilities.dart';
 
-abstract class LayoutConfig {
-  static double? deviceHeight;
-  static double? deviceWidth;
-  static MediaQueryData? mediaQueryData;
-
-  void init(BuildContext context) {
-    mediaQueryData = MediaQuery.of(context);
-    deviceWidth = mediaQueryData!.size.width;
-    deviceHeight = mediaQueryData!.size.height;
-  }
-}
-
 abstract class SpaceConfig {
   static double shortSpace = 8;
   static double normalSpace = 12;
