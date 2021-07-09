578f600db253d383528373482f4e1a8b5404f051
Nailul Firdaus <nail.nafir@gmail.com>
2021-07-09 16:46:33 +0700

    fix text style in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -88,11 +88,20 @@ class _HomePageState extends State<HomePage> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text("PANTAU COVID-19",
-                      style: TypeTheme.normalTextFont.copyWith(
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.coronavirus_outlined,
                         color: Colors.white,
-                        fontWeight: FontWeight.w500,
-                      )),
+                      ),
+                      SizedBox(width: 4),
+                      Text("PANTAU COVID-19",
+                          style: TypeTheme.normalTextFont.copyWith(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          )),
+                    ],
+                  ),
                   Icon(
                     Icons.notifications_outlined,
                     color: Colors.white,
