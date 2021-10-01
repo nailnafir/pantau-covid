93900db47110175ec514f1f676a81ac48e02d182
Nailul Firdaus <nail.nafir@gmail.com>
2021-10-02 05:57:46 +0700

    change url for vaccine in shared

--- lib/utilities/shared.dart
+++ lib/utilities/shared.dart
@@ -4,6 +4,6 @@ abstract class URLShared {
   static String telegramURL = "https://t.me/nailnafir";
   static String emailURL = "mailto:nailnafir@outlook.com";
   static String apiLocalURL = 'https://data.covid19.go.id';
-  static String apiVaccineURL = 'https://vaksincovid19-api.vercel.app';
+  static String apiVaccineURL = 'https://cekdiri.id';
   static String apiGlobalURL = 'https://covid19.mathdro.id/api/countries/';
 }
