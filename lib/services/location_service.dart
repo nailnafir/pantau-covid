212cdfa3ebb76b470ba718a51006a53c3f093d1f
Nailul Firdaus <nail.nafir@gmail.com>
2021-12-14 08:59:18 +0700

    change info location service

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -9,7 +9,7 @@ class LocationService {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
+        return Future.error('Izin lokasi ditolak');
       }
     }
 
@@ -21,9 +21,11 @@ class LocationService {
       List<Placemark> placemarks =
           await placemarkFromCoordinates(position.latitude, position.longitude);
       print(placemarks[0]);
+
       List<String> subAdministrativeList =
           placemarks[0].subAdministrativeArea!.split(" ");
       List<String> localityList = placemarks[0].locality!.split(" ");
+
       return LocationModel(
           address: "${localityList[1]}, ${subAdministrativeList[1]}.");
     } catch (e) {
